03ca5ec2978249f6a2842cae63926f7ddcd2d551
PaoDerass <paola_deras@unitec.edu>
2024-09-12 14:15:30 -0600

    agregar componentes

--- Modelo/tarea.cs
+++ Modelo/tarea.cs
@@ -1,10 +1,15 @@
 ﻿
 using SQLite;
+
+namespace ToDoList.modelo
+{ 
 public class tarea
 {
     public int id { get; set; }
     public string titulo { get; set; }
     public string descripcion { get; set; }
     public string status { get; set; }
+    public bool Completada { get; set; }
 
+}
 }
