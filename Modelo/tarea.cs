f33e8f2d8d84e1428e5c9283b54972e1172bde11
PaoDerass <paola_deras@unitec.edu>
2024-09-12 14:09:30 -0600

    creacion clase tarea

---
+++ Modelo/tarea.cs
@@ -0,0 +1,10 @@
+﻿
+using SQLite;
+public class tarea
+{
+    public int id { get; set; }
+    public string titulo { get; set; }
+    public string descripcion { get; set; }
+    public string status { get; set; }
+
+}
