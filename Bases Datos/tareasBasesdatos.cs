c3badca12da824462f9e35c49a4b49e21fdbf66f
PaoDerass <paola_deras@unitec.edu>
2024-09-13 09:34:01 -0600

    creacion de los calculos en la base de datos

---
+++ Bases Datos/tareasBasesdatos.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using ToDoList.modelo;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoList.Data
+{
+    public class TaskDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+
+        public TaskDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<tarea>().Wait();
+        }
+
+        public Task<List<tarea>> GetTasksAsync()
+        {
+            return _database.Table<tarea>().ToListAsync();
+        }
+
+        public Task<int> SaveTaskAsync(tarea task)
+        {
+            if (task.id != 0)
+            {
+                return _database.UpdateAsync(task);
+            }
+            else
+            {
+                return _database.InsertAsync(task);
+            }
+        }
+
+        public Task<int> DeleteTaskAsync(tarea task)
+        {
+            return _database.DeleteAsync(task);
+        }
+    }
+
+}
