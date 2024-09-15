c906c7048296cd869cafd2140a7184182354c77d
PaoDerass <paola_deras@unitec.edu>
2024-09-15 12:10:35 -0600

    solucion de errores en main.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,14 @@
-﻿namespace TodoList_PaolaDeras
+﻿using ToDoList.VistaModelo;
+
+namespace TodoList_PaolaDeras
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            
+            BindingContext = new VistaModelo(); 
         }
     }
-
 }
